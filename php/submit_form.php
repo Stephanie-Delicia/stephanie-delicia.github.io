46aaa732c279d8b6a6b04e47dfc9e54ed50f2889
Stephanie Martinez <91435723+Stephanie-Delicia@users.noreply.github.com>
2024-08-23 14:00:29 -0400

    Add files via upload

--- php/submit_form.php
+++ php/submit_form.php
@@ -1,16 +1,16 @@
 <?php
 $action=$_REQUEST['action'];
-if ($action=="") /* display the contact form */
+if ($action=="") /* if no action made, just display */
     {
     ?>
     <form action="" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="action" value="submit">
     Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
+    <input name="name" type="text" value=""/><br>
     Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
+    <input name="email" type="text" value=""/><br>
     Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
+    <textarea name="message" rows="5"></textarea><br>
     <input type="submit" value="Send email"/>
     </form>
     <?php
@@ -24,10 +24,10 @@ else /* send the submitted data */
         {
         echo "All fields are required, please fill <a href=\"\">the form</a> again.";
         }
-    else{        
+    else {        
         $from="From: $name<$email>\r\nReturn-path: $email";
         $subject=" Message sent using your contact form";
-        mail("youremail@yoursite.com", $subject, $message, $from);
+        mail("smartinezdelicia@gmail.com", $subject, $message, $from);
         echo "Email sent!";
         }
     }  
