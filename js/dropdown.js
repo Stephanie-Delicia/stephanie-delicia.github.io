51a6d16e6886bb2fae4e5e88c47c57abad0765b1
Stephanie Martinez <91435723+Stephanie-Delicia@users.noreply.github.com>
2024-10-22 01:45:16 -0400

    Add files via upload

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,26 @@
 function dropDownClicked() {
     document.getElementById("dropdown1").classList.toggle("show");
+    document.getElementById("dropdown2").classList.toggle("show2");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-btn')) {
+    if (event.target == document.getElementById('dropdown-btn')) {
+      console.log("first button clicked");
       var dropdown = document.getElementById("dropdown1");
       if (dropdown.classList.contains('show')) {
         document.getElementById("dropdown-img-examples").style.display = 'none';
       } else {
         document.getElementById("dropdown-img-examples").style.display = 'inline-block';
       }
+    } else if (event.target == document.getElementById('dropdown-btn2')) { // second button
+        console.log("second button clicked");
+        var dropdown = document.getElementById("dropdown2");
+        if (dropdown.classList.contains('show2')) {
+          document.getElementById("dropdown-class-diagram").style.display = 'none';
+        } else {
+          document.getElementById("dropdown-class-diagram").style.display = 'inline-block';
+        }
     }
+
   }
